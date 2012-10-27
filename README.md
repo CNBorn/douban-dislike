e13f0c11dc04925cd8a5ec1aa64a0f0378c7ceba
CNBorn <CNBorn@hotmail.com>
2012-10-27 21:44:33 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# 豆瓣小组管理助手 - Chrome插件
+
+## Features:
+### 封禁并删除
+封禁发帖用户并转向删除帖子页面，简化了封禁操作
+
+### 找出僵尸用户
+在用户列表中标注出僵尸用户（名号为“[已注销]”的用户），方便清理
+_Notice: 此功能不再被支持，由于豆瓣小组管理已经做了“扫描/清理已注销成员”功能_
+
+### 快速退出小组
+在“[我参加的小组](http://www.douban.com/group/mine)”页面快速退出小组
+
+[Chrome store link](https://chrome.google.com/webstore/detail/jfgffepggleibocpdkgfkgnkickjoham)
---
+++ douban_dislike.js
@@ -0,0 +1,39 @@
+var remove_site_hot_content = function(){
+    $("div.guess-item:not([id^='1087031'])").remove();
+}
+
+var refresh_guess_items_and_unread_count = function(){
+    var douban_home_link = $("div.site-nav-items ul li:eq(0) a");
+    var unread_count = $("div.guess-item").length;
+    douban_home_link.text("首页(" + unread_count + ")");
+}
+
+var put_dislike_button = function() {
+    $("div.guess-item div.ft").append('<span class="usr-btn fav-btn dislike-btn"><a href>不喜欢</a></span>');
+}
+
+var put_dismiss_buttion = function() {
+    $("div.guess-item div.ft").append('<span class="usr-btn dismiss-btn"><a href>隐藏</a></span>');
+
+    $("div.guess-item").delegate("div.ft span.dismiss-btn a", "click", function() {
+	$(this).parent().parent().parent().fadeOut().remove(); //div.guess-item
+
+	var refresh_guess_items_and_unread_count = function(){
+	    var douban_home_link = $("div.site-nav-items ul li:eq(0) a");
+	    var unread_count = $("div.guess-item").length;
+	    douban_home_link.text("首页(" + unread_count + ")");
+	}
+	refresh_guess_items_and_unread_count();
+	return false;
+    });
+
+}
+
+var load_more_guess = function() {
+    $("div.guess-more a").click();
+}
+
+remove_site_hot_content();
+refresh_guess_items_and_unread_count();
+put_dislike_button();
+put_dismiss_buttion();
